3e8bcafe2aad9ffc162827a08b5552e9879f165d
MohamedElhadidy0019 <Mohamed_Khaled_Kamal@outlook.com>
2020-06-08 07:37:35 +0200

    working now

--- stats.c
+++ stats.c
@@ -9,14 +9,13 @@
  *
  *****************************************************************************/
 /**
- * Mohamed github
- * tessting again
+ * 
  * @file Stats.c
- * @brief The code for the first assignment
- * <Add Extended Description Here>
+ * @brief 
+ * just testing the functions
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Mohamed Khaled Elhadidy
+ * @date 6/8/2020
  *
  */
 
@@ -30,15 +29,87 @@
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+    unsigned char test[SIZE] = { 34, 201 , 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  
+  print_statisitcs(test,SIZE);
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+  return;
 
 }
 
-/* Add other Implementation File Code Here */
+//Implemantation of the functions in the .h file
+void print_statisitcs(unsigned char* arr,int size)
+{
+  printf("minimum=%d\nmaximum=%d\nmean=%f\nmedian=%d\n",find_minimum(arr,size),find_maximum(arr,size),find_mean(arr,size),find_median(arr,size));
+}
+void print_array(unsigned char* arr,int size)
+{
+  printf("array= ");
+  for(int i=0;i<size;i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+}
+int find_median(unsigned char* arr,int size)
+{
+  if (size%2==0)
+  {
+    return arr[(size/2)-1];
+  }
+  return arr[(size/2)];
+}
+float find_mean(unsigned char* arr,int size)
+{
+  int sum=0;
+  for(int i=0;i<size;i++)
+  {
+    sum+=arr[i];
+  }
+  return (sum/(float)size);
+}
+int find_maximum(unsigned char* arr,int size)
+{
+  int max=arr[0];
+  for(int i=1;i<size;i++)
+  {
+    if(arr[i]>max)
+    {
+      max=arr[i];
+    }
+  }
+  return max;
+}
+int find_minimum(unsigned char* arr,int size)
+{
+  int min=arr[0];
+  for(int i=1;i<size;i++)
+  {
+    if(arr[i]<min)
+    {
+      min=arr[i];
+    }
+  }
+  return min;
+}
+void sort_array(unsigned char* arr,int size)
+{
+  for(int i=0;i<size;i++)
+  {
+    for(int j=i;j<size;j++)
+    {
+      if(arr[i]>arr[j])
+      {
+          //The swap process
+        unsigned char temp = arr[i]; //temp to save in it the arr[i]
+        arr[i]=arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
--- stats.h
+++ stats.h
@@ -22,25 +22,80 @@
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
-void print_statisitcs(int*,int);
-void print_array(int*,int);
-int find_median(int*,int);
-int find_mean(int*,int);
-int find_maximum(int*,int);
-int find_minimum(int*,int);
-void sort_array(int*,int);
+void print_statisitcs(unsigned char*,int);
+void print_array(unsigned char*,int);
+int find_median(unsigned char*,int);
+float find_mean(unsigned char*,int);
+int find_maximum(unsigned char*,int);
+int find_minimum(unsigned char*,int);
+void sort_array(unsigned char*,int);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief  print_statisitics 
+ * 
+ * prints all the infoormation about the given array
  *
- * <Add Extended Description Here>
+ * @param  pointer to the first element of the array
+ * @param  size of the array
+ * 
+ * @return  nohting it only prints so it is void
+ * 
+ * * @brief   print_array
+ *  prints all the elements of the arrat
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param  pointer to the first element of the array
+ * @param  size of the array
+ * 
+ * @return  nothing it is void
+ 
+ * @brief   find_median 
+ *  gets the median of the given array
  *
- * @return <Add Return Informaiton here>
+ * @param  pointer to the first element of the array
+ * @param  size of the array
+ * 
+ * @return the median as an integer type
+ 
+
+ * @brief  find_mean
+ * 
+ *finds the mean of the given array
+ * @param  pointer to the first element of the array
+ * @param  size of the array
+ * 
+ * @return the mean of the array(but float not integer as it may be a fraction)
+ 
+
+ * @brief  find_maximum
+ * 
+ *
+ * @param  pointer to the first element of the array
+ * @param  size of the array
+ * 
+ * @return  the maximux value of the array as an int
+ 
+
+ * @brief  find_minmum
+ * 
+ *
+ * @param  pointer to the first element of the array
+ * @param  size of the array
+ * 
+ * @return  the minimum value in the array as an int
+ 
+
+ * @brief  sort_array
+ * 
+ *sorts the array in ascending order
+ * @param  pointer to the first element of the array
+ * @param  size of the array
+ * 
+ * @return nothing as it is void
+ 
+
+ * 
+ * 
+ 
  */
 
 
